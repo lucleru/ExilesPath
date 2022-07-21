02558504677ea608426ed9f811487b2771282bde
lclemenr <lclemenr@everis.com>
2022-07-21 11:25:23 +0200

    first commit

---
+++ src/main/java/com/lucleru/ExilesPath/ExilesPathApplication.java
@@ -0,0 +1,13 @@
+package com.lucleru.ExilesPath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExilesPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExilesPathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucleru/ExilesPath/ExilesPathTests.java
@@ -0,0 +1,13 @@
+package com.lucleru.ExilesPath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExilesPathTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
